16cf5aff73b4a6daccf578e736a0ed2bc07419fc
siqister <siqi.zhu@gmail.com>
2017-02-26 11:35:52 -0500

    first commit

--- public/3-animation-basics/app.js
+++ public/3-animation-basics/app.js
@@ -20,13 +20,34 @@ var Point = function(){
 	return {
 		x:Math.random()*w,
 		y:Math.random()*h,
-		speedX: Math.random()*.05,
-		speedY: Math.random()*.05,
+		speedX: Math.random()*.5+1,
+		speedY: Math.random()*.5+1,
 		update: function(){
-			console.log(this);
+			if((this.x+this.speedX)>w || (this.x+this.speedX)<0) this.speedX=-1*this.speedX;
+			if((this.y+this.speedY)>h || (this.y+this.speedY)<0) this.speedY = -1*this.speedY;
+			this.x += this.speedX;
+			this.y += this.speedY;
 		}
 	}
 }
 
 var point1 = Point();
-point1.update();
+
+redraw();
+
+function redraw(){
+	//Start fresh...
+	ctx1.clearRect(0,0,w,h);
+	ctx1.fillStyle = 'rgba(0,0,0,.5)';
+
+	//Draw dot representing point1
+	ctx1.beginPath();
+	ctx1.arc(point1.x,point1.y,5,0,Math.PI*2);
+	ctx1.fill();
+
+	//Update point1 for the next go-around
+	point1.update();
+
+	//Request next frame/redraw
+	requestAnimationFrame(redraw);
+}
